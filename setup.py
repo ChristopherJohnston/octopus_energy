237929e736d370d70edee2450189e38abf298e73
Christopher Johnston <christopher_johnston@me.com>
2022-01-17 10:58:18 +0000

    moved package to src directory, as per pypi guidelines

--- setup.py
+++ setup.py
@@ -12,11 +12,14 @@ PACKAGE_DIR = (HERE/"src")
 # This call to setup() does all the work
 setup(
     name="octopus-energy-client",
-    version="0.0.10",
+    version="0.0.11",
     description="A Python API for retrieving Octopus Energy data",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/ChristopherJohnston/octopus_energy",
+    project_urls={
+        "Bug Tracker": "https://github.com/ChristopherJohnston/octopus_energy/issues"
+    },
     author="Christopher P Johnston",
     author_email="c.p.johnston@gmail.com",
     license="MIT",
@@ -25,7 +28,9 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ],
-    packages=find_packages(),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     include_package_data=True,
-    install_requires=["requests", "pytz"]
+    install_requires=["requests", "pytz"],
+    python_requires=">=3.6"
 )
